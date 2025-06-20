3e95f5b3481cff81f2a0f84937db3de2feebdf00
Hernan Alvarado <hernanvid123@gmail.com>
2025-06-20 15:41:32 -0500

    feat(hook): add `useToggle` hook

---
+++ src/use-toggle.ts
@@ -0,0 +1,20 @@
+"use client"
+import { useState } from "react"
+
+/**
+ * useToggle is a React hook that manages a boolean toggle state.
+ *
+ * @param {boolean} initial - Initial state of the toggle, defaults to false.
+ * @returns {Function} - Returns the current toggle state and a function to update it.
+ * @example
+ * const [isToggled, toggle] = useToggle();
+ */
+export const useToggle = (initial: boolean = false): [boolean, (value: boolean) => void] => {
+    const [toggled, setToggled] = useState(initial)
+
+    const handleToggle = (value: boolean) => {
+        setToggled(value)
+    }
+
+    return [toggled, handleToggle]
+}
