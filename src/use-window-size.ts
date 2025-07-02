de8a07a9f6d9c9d23968af75d5efb1edb0f5398c
Hernan Alvarado <hernanvid123@gmail.com>
2025-07-02 15:38:24 -0500

    feat(hooks): add `useWindowSize` hook (#6)
    * feat(hooks): add `useWindowSize` hook
    
    * chore(hooks): use `useLayoutEffect` hook

---
+++ src/use-window-size.ts
@@ -0,0 +1,40 @@
+import { useLayoutEffect, useState } from "react"
+
+/**
+ * Represents the size of the window with width and height properties.
+ */
+interface WindowSize {
+    width: number
+    height: number
+}
+
+/**
+ * Gets the current window size of the browser window giving the width and height values,
+ * this values are updated on resize events.
+ *
+ * @returns {WindowSize} - The current window size.
+ * @example
+ * const { width, height } = useWindowSize();
+ */
+export const useWindowSize = (): WindowSize => {
+    const [windowSize, setWindowSize] = useState<WindowSize>({
+        width: 0,
+        height: 0,
+    })
+
+    useLayoutEffect(() => {
+        const handleResize = () => {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            })
+        }
+
+        handleResize()
+        document.addEventListener("resize", handleResize)
+
+        return () => document.removeEventListener("resize", handleResize)
+    }, [])
+
+    return windowSize
+}
