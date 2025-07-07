bb27db3a5f7f3e3a7d12077b1b15e5cfbf558cf1
Hernan Alvarado <hernanvid123@gmail.com>
2025-07-06 21:58:28 -0500

    chore(hooks): enhance `useLocalStorage` hook

--- src/use-localstorage.ts
+++ src/use-localstorage.ts
@@ -1,26 +1,80 @@
 "use client"
-import { useState, useEffect } from "react"
+import { useState, useCallback, useEffect } from "react"
 
-export const useLocalStorage = <T>(key: string, initialValue?: T) => {
-    const [storage, setStorage] = useState<T>()
+export const defaultSerializer = {
+    serialize: JSON.stringify,
+    deserialize: JSON.parse,
+}
 
-    const setValue = (value: T | ((previous: undefined | T) => T)) => {
-        const valueToStore = value instanceof Function ? value(storage) : value
-        const storageValue = JSON.stringify(valueToStore)
-        setStorage(valueToStore)
-        localStorage.setItem(key, storageValue)
+interface UseLocalStorageOptions<T> {
+    serializer?: {
+        serialize: (value: T) => string
+        deserialize: (value: string) => T
     }
+    withEvent?: boolean
+}
 
-    const getStorage = () => {
+interface UseLocalStorageReturn<T> {
+    storage: T | undefined
+    setValue: (value: T | ((previous: undefined | T) => T)) => void
+    removeItem: () => void
+}
+
+export const useLocalStorage = <T>(key: string, initialValue?: T, options: UseLocalStorageOptions<T> = {}) => {
+    const [storage, setStorage] = useState<T | undefined>(initialValue)
+
+    const { withEvent = false, serializer = defaultSerializer } = options
+    const { serialize, deserialize } = serializer
+
+    const isBrowser = useCallback((): boolean => {
+        return typeof window !== "undefined" && typeof window.localStorage !== "undefined"
+    }, [])
+
+    const setValue: UseLocalStorageReturn<T>["setValue"] = useCallback(
+        (value) => {
+            if (!isBrowser()) return
+            setStorage((previous) => {
+                const valueToStore = value instanceof Function ? value(previous) : value
+                const storageValue = serialize(valueToStore)
+                localStorage.setItem(key, storageValue)
+                return valueToStore
+            })
+        },
+        [key, serializer],
+    )
+
+    const getStorage = useCallback(() => {
+        if (!isBrowser()) return
         const getItem = localStorage.getItem(key)
-        const storageValue = getItem ? JSON.parse(getItem) : initialValue
+        const storageValue = getItem ? deserialize(getItem) : initialValue
+        localStorage.setItem(key, serialize(storageValue))
         setStorage(storageValue)
-        localStorage.setItem(key, JSON.stringify(storageValue))
-    }
+        return storageValue
+    }, [key, initialValue, serializer])
+
+    const removeItem: UseLocalStorageReturn<T>["removeItem"] = useCallback(() => {
+        setStorage(() => undefined)
+        localStorage.removeItem(key)
+    }, [key, isBrowser])
 
     useEffect(() => {
         getStorage()
-    }, [])
+    }, [key, serializer])
 
-    return [storage, setValue] as const
+    useEffect(() => {
+        if (!withEvent || !isBrowser()) return
+
+        const syncStorage = (event: StorageEvent) => {
+            const { key, newValue } = event
+            if (event.key === key) {
+                const newContextValue = newValue ? deserialize(newValue) : undefined
+                setStorage(newContextValue)
+            }
+        }
+
+        window.addEventListener("storage", syncStorage)
+        return () => window.removeEventListener("storage", syncStorage)
+    }, [key, serializer, withEvent, isBrowser])
+
+    return [storage, setValue, removeItem]
 }
