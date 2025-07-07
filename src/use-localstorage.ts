93113131c094e41b87f9b33b6f567726ed8dd720
Hernan Alvarado <hernanvid123@gmail.com>
2025-07-06 22:05:32 -0500

    docs(hook): add documentation via JSDocs

--- src/use-localstorage.ts
+++ src/use-localstorage.ts
@@ -1,11 +1,21 @@
 "use client"
 import { useState, useCallback, useEffect } from "react"
 
+/**
+ * Default serializer for `useLocalStorage` hook
+ */
 export const defaultSerializer = {
     serialize: JSON.stringify,
     deserialize: JSON.parse,
 }
 
+/**
+ * Options for the third argument of `useLocalStorage` hook
+ * - `serializer`: Custom serializer for the value stored in localStorage.
+ * - `withEvent`: If true, the hook will listen to the `storage` event
+ *   to synchronize the value across different tabs or windows.
+ *   This is useful when the localStorage is modified in another tab.
+ */
 interface UseLocalStorageOptions<T> {
     serializer?: {
         serialize: (value: T) => string
@@ -14,12 +24,40 @@ interface UseLocalStorageOptions<T> {
     withEvent?: boolean
 }
 
+/**
+ * Return type of `useLocalStorage` hook
+ * - `storage`: The current value stored in localStorage.
+ * - `setValue`: Function to set a new value in localStorage.
+ * - `removeItem`: Function to remove the item from localStorage.
+ */
 interface UseLocalStorageReturn<T> {
     storage: T | undefined
     setValue: (value: T | ((previous: undefined | T) => T)) => void
     removeItem: () => void
 }
 
+/**
+ * `useLocalStorage` is a React hook that provides a way to manage state synchronized with the browser's localStorage.
+ * It allows you to set, get, and remove items from localStorage while keeping the state in sync with the stored value.
+ *
+ * @param {string} key - The key under which the value is stored in localStorage.
+ * @param {T} initialValue - The initial value to be stored in localStorage. If not provided, it defaults to `undefined`.
+ * @param {UseLocalStorageOptions<T>} options - Options for customizing the behavior of the hook.
+ * @example
+ * const [value, setValue, removeItem] = useLocalStorage("myKey", "defaultValue", {
+ *     withEvent: true,
+ *     serializer: {
+ *         serialize: (value) => JSON.stringify(value),
+ *         deserialize: (value) => JSON.parse(value),
+ *     },
+ * });
+ *
+ * // Sets the value in localStorage and updates the state
+ * setValue("newValue");
+ *
+ * // Removes the item from localStorage and updates the state
+ * removeItem();
+ */
 export const useLocalStorage = <T>(key: string, initialValue?: T, options: UseLocalStorageOptions<T> = {}) => {
     const [storage, setStorage] = useState<T | undefined>(initialValue)
 
