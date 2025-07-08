510bd85d344fb7eeb50488b19d587142d5021205
Hernan Alvarado <hernanvid123@gmail.com>
2025-07-07 22:28:33 -0500

    feat(hooks): add `useBroadcastChannel` hook

---
+++ src/use-broadcast-channel.ts
@@ -0,0 +1,51 @@
+"use client"
+import { useState, useEffect, useRef, useCallback } from "react"
+
+/**
+ * Return type for the `useBroadcastChannel` hook.
+ */
+type BroadcastChannelReturn<T> = [T | undefined, (message: T) => void]
+
+/**
+ * `useBroadcastChannel` is a React hook that provides a way to communicate between different tabs or windows of
+ * the same origin using the BroadcastChannel API.
+ *
+ * @param {string} name - The name of the BroadcastChannel
+ * @param {T} initialValue - The initial value to set in the channel
+ * @returns {BroadcastChannelReturn<T>} - An array containing the current state and a function to send messages
+ * @example
+ * const [message, sendMessage] = useBroadcastChannel<string>("my-channel", "Hello World");
+ */
+export const useBroadcastChannel = <T>(name: string, initialValue?: T): BroadcastChannelReturn<T> => {
+    const [state, setState] = useState<T | undefined>(initialValue)
+    const broadcastChannel = useRef<BroadcastChannel | undefined>(undefined)
+    const isSupported = typeof window !== "undefined" && "BroadcastChannel" in window
+
+    const sendMessage = useCallback(
+        async (message: T) => {
+            if (!isSupported) return
+            broadcastChannel.current?.postMessage(message)
+            setState(message)
+        },
+        [name, isSupported],
+    )
+
+    useEffect(() => {
+        if (!isSupported) return
+        broadcastChannel.current ??= new BroadcastChannel(name)
+
+        const syncBroadcast = (event: MessageEvent) => {
+            setState(event.data)
+        }
+
+        broadcastChannel.current.addEventListener("message", syncBroadcast)
+
+        return () => {
+            broadcastChannel.current?.removeEventListener("message", syncBroadcast)
+            broadcastChannel.current?.close()
+            setState(undefined)
+        }
+    }, [])
+
+    return [state, sendMessage]
+}
