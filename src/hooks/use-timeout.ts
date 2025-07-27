ebe243d239b45670f14e9c76ea1f94db5e43406e
Hernan Alvarado <hernanvid123@gmail.com>
2025-07-27 22:45:26 +0000

    feat(hooks): use `useTimeout` hook

---
+++ src/hooks/use-timeout.ts
@@ -0,0 +1,20 @@
+"use client"
+import type { UseTimeout } from "@/types"
+import { useEffect, useRef } from "react"
+
+/**
+ * `useTimeout` is a custom hook that runs a callback after the provided milliseconds.
+ * 
+ * @param {VoidFunction} callback - The function to be called after the delay
+ * @param {number | undefined | null} delay - The delay in milliseconds. If null, the timeout is not set.
+ */
+export const useTimeout: UseTimeout = (callback, delay) => {
+    const callbackRef = useRef<VoidFunction>(callback)
+
+    useEffect(() => {
+        const defaultTime = delay ?? 0
+        const idle = setTimeout(callbackRef.current, defaultTime)
+
+        return () => clearTimeout(idle)
+    }, [delay])
+}
